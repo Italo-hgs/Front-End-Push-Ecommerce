c4efc319978e159b4de5367dbed1e956f559db17
Italo Góes <70420128+Italo-hgs@users.noreply.github.com>
2022-10-26 17:52:11 -0300

    Add files via upload

---
+++ desafio push ecommerce/assets/script/script.js
@@ -0,0 +1,13 @@
+function exibeMenuMobile(){
+    let menuMobile = document.querySelector('.menu-mobile');
+    if(menuMobile.style.display = 'none'){
+        menuMobile.style.display = 'block';
+    }
+}
+
+function ocutaMenuMobile(){
+    let menuMobile = document.querySelector('.menu-mobile');
+    if(menuMobile.style.display = 'block'){
+        menuMobile.style.display = 'none';
+    }
+}
